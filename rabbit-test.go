4bdffecabe14579ed8924a0506cbfffff1e25613
Joan Oliva <joliva@oneboxtm.com>
2015-11-23 18:07:43 +0100

    test consume from rabbit mq topic ok

---
+++ rabbit-test.go
@@ -0,0 +1,173 @@
+// This example declares a durable Exchange, an ephemeral (auto-delete) Queue,
+// binds the Queue to the Exchange with a binding key, and consumes every
+// message published to that Exchange with that routing key.
+//
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	//	"net/http"
+
+	"code.google.com/p/go.net/websocket"
+	"github.com/streadway/amqp"
+)
+
+var (
+	uri          = flag.String("uri", "amqp://guest:guest@192.168.99.100:5672/", "AMQP URI")
+	exchange     = flag.String("exchange", "logs", "Durable, non-auto-deleted AMQP exchange name")
+	exchangeType = flag.String("exchange-type", "direct", "Exchange type - direct|fanout|topic|x-custom")
+	queue        = flag.String("queue", "logs", "Ephemeral AMQP queue name")
+	bindingKey   = flag.String("key", "logs", "AMQP binding key")
+	consumerTag  = flag.String("consumer-tag", "simple-consumer", "AMQP consumer tag (should not be blank)")
+	wsPort       = flag.Int("ws-port", 8080, "WebSockets port to listen to")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func transmitLog(ws *websocket.Conn) {
+	// Here, send to the browser what the AMQP Consumer got.
+}
+
+func main() {
+	/*
+		log.Println("Starting application on port 8080...")
+		err := http.ListenAndServe(fmt.Sprintf(":%d", *wsPort), nil)
+		if err != nil {
+			log.Fatalf("listen: %s", err)
+		}
+
+		http.Handle("/", websocket.Handler(transmitLog))
+	*/
+	c, err := NewConsumer(*uri, *exchange, *exchangeType, *queue, *bindingKey, *consumerTag)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	select {}
+
+	if err := c.Shutdown(); err != nil {
+		log.Fatalf("error during shutdown: %s", err)
+	}
+
+}
+
+type Consumer struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	tag     string
+	done    chan error
+}
+
+func NewConsumer(amqpURI, exchange, exchangeType, queue, key, ctag string) (*Consumer, error) {
+	c := &Consumer{
+		conn:    nil,
+		channel: nil,
+		tag:     ctag,
+		done:    make(chan error),
+	}
+	log.Println("New rabbitmq consumer created")
+
+	var err error
+
+	log.Printf("dialing %s", amqpURI)
+	c.conn, err = amqp.Dial(amqpURI)
+	if err != nil {
+		return nil, fmt.Errorf("Dial: %s", err)
+	}
+
+	log.Printf("got Connection, getting Channel")
+	c.channel, err = c.conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("Channel: %s", err)
+	}
+
+	log.Printf("got Channel, declaring Exchange (%s)", exchange)
+	if err = c.channel.ExchangeDeclare(
+		exchange,     // name of the exchange
+		exchangeType, // type
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
+	); err != nil {
+		return nil, fmt.Errorf("Exchange Declare: %s", err)
+	}
+
+	log.Printf("declared Exchange, declaring Queue (%s)", queue)
+	state, err := c.channel.QueueDeclare(
+		queue, // name of the queue
+		true,  // durable
+		false, // delete when usused
+		false, // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Queue Declare: %s", err)
+	}
+
+	log.Printf("declared Queue (%d messages, %d consumers), binding to Exchange (key '%s')",
+		state.Messages, state.Consumers, key)
+
+	if err = c.channel.QueueBind(
+		queue,    // name of the queue
+		key,      // bindingKey
+		exchange, // sourceExchange
+		false,    // noWait
+		nil,      // arguments
+	); err != nil {
+		return nil, fmt.Errorf("Queue Bind: %s", err)
+	}
+
+	log.Printf("Queue bound to Exchange, starting Consume (consumer tag '%s')", c.tag)
+	deliveries, err := c.channel.Consume(
+		queue, // name
+		c.tag, // consumerTag,
+		false, // noAck
+		false, // exclusive
+		false, // noLocal
+		false, // noWait
+		nil,   // arguments
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Queue Consume: %s", err)
+	}
+
+	go handle(deliveries, c.done)
+
+	return c, nil
+}
+
+func (c *Consumer) Shutdown() error {
+	// will close() the deliveries channel
+	if err := c.channel.Cancel(c.tag, true); err != nil {
+		return fmt.Errorf("Consumer cancel failed: %s", err)
+	}
+
+	if err := c.conn.Close(); err != nil {
+		return fmt.Errorf("AMQP connection close error: %s", err)
+	}
+
+	defer log.Printf("AMQP shutdown OK")
+
+	// wait for handle() to exit
+	return <-c.done
+}
+
+func handle(deliveries <-chan amqp.Delivery, done chan error) {
+	for d := range deliveries {
+		log.Printf(
+			"got %dB delivery: [%v] %s",
+			len(d.Body),
+			d.DeliveryTag,
+			d.Body,
+		)
+	}
+	log.Printf("handle: deliveries channel closed")
+	done <- nil
+}
